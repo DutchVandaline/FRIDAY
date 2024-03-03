e0973b74b9807b55cce8e12d4cebe1f12f397f9f
Jason Park <142364450+DutchVandaline@users.noreply.github.com>
2024-03-03 21:17:29 +0900

    Update friday_voice.py

--- Friday_code/friday_voice.py
+++ Friday_code/friday_voice.py
@@ -1,5 +1,8 @@
 import pygame
 import threading
+from auto_delete_audio import delete_old_files
+from datetime import datetime
+from colorama import Fore
 
 class FridayVoice:
     def __init__(self, output_audio):
@@ -12,7 +15,10 @@ class FridayVoice:
         pygame.mixer.music.play()
 
     def wait_for_input(self):
-        input()
+        try:
+            input()
+        except EOFError:
+            pass
     
     def stop(self):
         pygame.mixer.music.stop()
@@ -24,3 +30,21 @@ class FridayVoice:
 
         music_thread.start()
         input_thread.start()
+
+    def speak_response(response, client):
+        print(Fore.YELLOW + "Response GPT-4")
+        print(Fore.YELLOW + response)
+        tts_response = client.audio.speech.create(
+                    model="tts-1",
+                    voice="nova",
+                    input=response,
+                )
+
+        current_time = datetime.now().strftime('%Y%m%d_%H%M%S')
+        output_file = f"Output_Audio/output_{current_time}.mp3"
+        tts_response.stream_to_file(output_file)
+
+        voice_player = FridayVoice(output_file)
+        voice_player.start()
+
+        delete_old_files("Output_Audio", 5)
